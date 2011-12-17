0bb43907e20fcd289703349c1c82817d5481d5e3
Ludger Heide <ludger.heide@gmail.com>
2011-12-17 22:00:28 +0100

    Licensed under WTFPL

--- Vigenere/AppDelegate.m
+++ Vigenere/AppDelegate.m
@@ -5,6 +5,19 @@
 //  Created by Ludger Heide on 06.11.11.
 //  Copyright (c) 2011 Ludger Heide. All rights reserved.
 //
+//DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//Version 2, December 2004
+//
+//Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+//
+//Everyone is permitted to copy and distribute verbatim or modified
+//copies of this license document, and changing it is allowed as long
+//as the name is changed.
+//
+//DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+//
+//0. You just DO WHAT THE FUCK YOU WANT TO.
 
 #import "AppDelegate.h"
 
