d9925a5027681d905568ad4c419a065b64af1a0f
Ludger Heide <ludger.heide@gmail.com>
2012-01-06 20:01:09 +0100

    Adjusted WTFPL.

--- Vigenere/AppDelegate.h
+++ Vigenere/AppDelegate.h
@@ -3,21 +3,12 @@
 //  Vigenere
 //
 //  Created by Ludger Heide on 06.11.11.
-//  Copyright (c) 2011 Ludger Heide. All rights reserved.
-//
-//DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-//Version 2, December 2004
-//
-//Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
-//
-//Everyone is permitted to copy and distribute verbatim or modified
-//copies of this license document, and changing it is allowed as long
-//as the name is changed.
-//
-//DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-//TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-//
-//0. You just DO WHAT THE FUCK YOU WANT TO.
+//  Copyright 2012 Ludger Heide.
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
 
 #import <UIKit/UIKit.h>
 
